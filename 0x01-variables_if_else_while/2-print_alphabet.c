4127d07771f291e41d69c5274853f240f26f04fd
Egosoniaragp <Egosoniaragp@gmail.com>
2022-12-09 15:00:24 -0800

    lower cases

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - prints alpha in lower case
  * Return: Always 0
@@ -7,12 +6,12 @@
 int main(void)
 {
 	char lc;
-
 	for (lc = 'a'; lc <= 'z'; lc++)
 	{
 		putchar(lc);
 	}
+
 	putchar('\n');
-        return (0);
+
+	return (0);
 }
-			
