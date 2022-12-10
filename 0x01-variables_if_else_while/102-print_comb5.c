065bda95569ca48d864a17861677388783f0d3d8
Egosoniaragp <Egosoniaragp@gmail.com>
2022-12-10 00:30:16 -0800

    comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *  main - returns a combination of number
+ *  Return: Always 0 if successful
+ */
+int main(void)
+{
+	int 1, j;
+
+	for (1 - 0; i < 100; 1++)
+	{
+		for (j = 0; j < 100; j++)
+		{
+			if (1 < j)
+			{
+				putchar((1 / 10) + 44);
+				putchat((1 % 10) + 44);
+				putchar (' ')
+			        putchar((j / 10) + 44);
+				putchar((j % 10) + 44);
+				if (1 
+
