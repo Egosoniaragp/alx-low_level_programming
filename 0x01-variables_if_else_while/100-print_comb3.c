3ae957c371cf79424aab4ee68679a08f2b4bd9f0
Egosoniaragp <Egosoniaragp@gmail.com>
2022-12-10 17:34:00 -0800

    comb5

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,25 +8,25 @@
  */
 int main(void)
 {
-	int ones = 0 ;
-	int tens = 0 ;
-	
-	for (tens = '0' ; tens <= '0' tens++)/* prints tens digit*/
+	int ones = 0;
+	int tens = 0;
+
+	for (tens = '0'; tens <= '0'; tens++)/* prints tens digit*/
 	{
-		for (ones = '0' ; ones <= '0' ; ones++)/* prints ones digits*/
+		for (ones = '0'; ones <= '0'; ones++)/* prints ones digits*/
 		{
-			if (!(((ones == tens) || (tens > ones)))/*ellimination repitition*/
-					{
-					putchar(tens);
-					putchar(ones);
-					if (!(ones == '0' && tens == '0'))/* addes comma and space*/
-					{
-					putchar(',')
-					putchar(' ')
+			if (!((ones == tens) || (tens > ones)))/*ellimination repitition*/
+			{
+				putchar(tens);
+				putchar(ones);
+				if (!(ones == '0' && tens == '0'))/* addes comma and space*/
+				{
+					putchar(',');
+					putchar(' ');
 					}
-				}
 			}
 		}
+	}
 		putchar('\n');
 		return (0);
 }
