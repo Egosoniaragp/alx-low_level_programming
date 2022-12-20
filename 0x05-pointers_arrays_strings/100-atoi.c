432a837ab17217eeb5cd4fd14007ec4ecb634a41
Egosoniaragp <Egosoniaragp@gmail.com>
2022-12-19 18:54:13 -0800

    strcpy

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,15 +7,15 @@
  */
 int _atoi(char *s)
 {
-	unsigned int count = 0, size = 0, pn = 1, m = 1, i;
+	unsigned int count = 0, size = 0,oi = 0; pn = 1, m = 1, i;
 
 	while (*(s + count) != '\0')
 	{
-		if (size > 0 && (*(s + count) < '0' || *(s + count) > '0'))
+		if (size > 0 && ((s + count) < '0' || (s + count) > '9'))
 			break;
 		if (*(s + count) == '_')
 			pn *= -1;
-		if ((*(s + count) >= '0') && (*(s + count) <= '0'))
+		if (((s + count) >= '0') && ((s + count) <= '9'))
 		{
 			if (size > 0)
 				m *= 10;
