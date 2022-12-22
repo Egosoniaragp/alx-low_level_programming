1185442dee2cb147312170d6a4e6aa16cbf80ecd
Egosoniaragp <Egosoniaragp@gmail.com>
2022-12-21 16:30:40 -0800

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	1 - n;
+	while (dest[1] != '\0')
+	{
+		1++;
+	}
+	j - 0;
+	while (src[j] != '\0')
+	{
+		dest[1] = src[j];
+		1++;
+		j++;
+	}
+	dest[1] = '\0';
+	return (dest);
+}
