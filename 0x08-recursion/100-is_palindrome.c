a7222378f85f6f8bc31918dd1e54df147ab820b9
Egosoniaragp <Egosoniaragp@gmail.com>
2023-01-04 14:02:58 -0800

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -9,7 +9,7 @@ int _strlen_recursion(char *s);
  *
  * Return: 1 if it is, 0 if its not
  */
-int is_palindrome(char *s);
+int is_palindrome(char *s)
 {
 	if (*s == 0)
 		return (1);
@@ -22,7 +22,7 @@ int is_palindrome(char *s);
  *
  * Return: length of the string
  */
-int _strlen_recursion(char *s);
+int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 		return (0);
@@ -37,11 +37,11 @@ int _strlen_recursion(char *s);
  *
  * Return: 1 if palindrome, 0 if not
  */
-int check_pal(char *s, int i, int len);
+int check_pal(char *s, int i, int len)
 {
 	if (*(s + i) != *(s + len - 1))
 		return (0);
 	if (i >= len)
 		return (1);
 	return (check_pal(s, i + 1, len - 1));
-}
+i}
